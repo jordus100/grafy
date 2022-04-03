61824272165a072ce48405badd0d90c0390017b1
Jordan Parviainen <jordus100@gmail.com>
2022-04-03 12:32:25 +0200

    added two structs to main.c
    The two structs are: 'graph' and 'vertex', had to to change the name of the latter from 'vertice' to be linguistically correct

--- src/graph_cohesion.h
+++ src/graph_cohesion.h
@@ -1,4 +1,6 @@
 #ifndef CGRAFY_GRAPH_COHESION_H
 #define CGRAFY_GRAPH_COHESION_H
 
+int isGraphCohesive(graph* thisGraph);
+
 #endif //CGRAFY_GRAPH_COHESION_H
--- src/main.c
+++ src/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+typedef struct graph{
+    int numberOfCols;
+    int numberOfRows;
+    vertex vertices[];
+};
+
+typedef struct vertex{
+    int vertexNumber;
+    double weightLeft;
+    double weightRight;
+    double weightUp;
+    double weightDown;
+};
+
 int main(int argc, char* argv[]){
     printf("Hello, world!");
 }
