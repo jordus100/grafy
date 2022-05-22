51189a6ad30dafe571fc3c1a44c723e457787cb7
Jordan Parviainen <jordus100@gmail.com>
2022-05-22 13:20:42 +0200

    fixed readGraphFromFile, it looks to be working

--- src/main/java/com/example/jgrafy/GraphGenerator.java
+++ src/main/java/com/example/jgrafy/GraphGenerator.java
@@ -2,32 +2,38 @@ package com.example.jgrafy;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 class GraphGenerator {
 
     public static Graph readGraphfromFile(String filePath) throws Exception {
-        Scanner sc = new Scanner(new File(filePath));
+        Scanner sc = new Scanner(new File(filePath)).useDelimiter(Pattern.compile("\\s+"));
         int numOfCols = Integer.parseInt(sc.next(Pattern.compile("\\d+")));
         int numOfRows = Integer.parseInt(sc.next(Pattern.compile("\\d+")));
         Graph graph = new Graph(numOfRows, numOfCols);
         //now reading the rows
         sc.nextLine();
         int vertexNum = 0;
-        while(sc.hasNext(Pattern.compile("(\\d+)\\s:(\\d).(\\d+)"))){
+        while(vertexNum <= graph.getNumOfRows()* graph.getNumOfColumns()){
             System.out.println(vertexNum);
             String connection = sc.findInLine(Pattern.compile("\\d+ :\\d.\\d+"));
             if(connection == null){
-                sc.nextLine();
+                try{
+                    sc.nextLine();
+                } catch (NoSuchElementException e){
+                    break;
+                }
                 vertexNum++;
                 continue;
             }
             String[] split = Pattern.compile(":").split(connection);
+            split[0] = split[0].trim();
+            split[1] = split[1].trim();
             int vertexConnection = Integer.parseInt(split[0]);
-            double weight = Integer.parseInt(split[1]);
+            double weight = Double.parseDouble(split[1]);
             graph.setVertice(vertexNum, graph.getDirection(vertexNum, vertexConnection), weight);
-
         }
         return graph;
     }
