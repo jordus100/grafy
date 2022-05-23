a4d7c0d41d9637a760c4ccd50a352cf425cca559
Szymon Posiadała <posiadalaszymon@gmail.com>
2022-05-23 18:42:09 +0200

    Dijkstra algorithm implementation added with test in main

--- src/main/java/com/example/jgrafy/Main.java
+++ src/main/java/com/example/jgrafy/Main.java
@@ -4,7 +4,7 @@ package com.example.jgrafy;
 public class Main {
 
     public static void main(String[] args) {
-        try {
+        /*try {
             Graph graph = GraphGenerator.readGraphFromFile("C:\\Users\\Jordu\\Documents\\Programming\\F1NextRace\\JGrafy\\data\\mygraph2");
             System.out.println(graph.getNumOfRows());
             System.out.println(graph.getNumOfColumns());
@@ -15,7 +15,15 @@ public class Main {
             }
             GraphAnalyzer.CohesionRef cohesionRef = GraphAnalyzer.checkCohesion(graph, 0);
             System.out.println("\n" + cohesionRef.cohesive);
-            graph.saveToFile("C:\\Users\\Jordu\\Documents\\Programming\\F1NextRace\\JGrafy\\data\\mygraph3");
+            //graph.saveToFile("C:\\Users\\Jordu\\Documents\\Programming\\F1NextRace\\JGrafy\\data\\mygraph3");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }*/
+        try {
+            Graph graph = GraphGenerator.readGraphFromFile("C:\\Users\\posia\\Desktop\\JIMP2\\test2.txt");
+            System.out.print("Wczytano \n");
+            GraphAnalyzer.findShortestPath(27,0,graph);
         }
         catch (Exception e){
             System.out.println(e.getMessage());
