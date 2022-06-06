c30a6cf3d3f6db1067a4956db73d034e75b3f07c
Jordan Parviainen <jordus100@gmail.com>
2022-06-06 18:09:00 +0200

    graph is now fully displayed, though not displaying paths yet

--- src/main/java/com/example/jgrafy/UIGraphViewController.java
+++ src/main/java/com/example/jgrafy/UIGraphViewController.java
@@ -6,11 +6,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+
+import java.text.DecimalFormat;
 
 public class UIGraphViewController {
 
@@ -24,48 +28,7 @@ public class UIGraphViewController {
         graphPane.prefHeightProperty().bind(graphScroll.heightProperty());
     }
 
-    private void addArrow(AnchorPane graphPane, double circleX, double circleY, double radius, Direction direction){
-        Line arrowBody = null;
-        Polygon arrowPoint = null;
-        arrowPoint = new Polygon();
-        if(direction == Direction.Right){
-            arrowBody = new Line(circleX + radius, circleY - 0.25*radius, circleX + 3*radius, circleY - 0.25*radius);
-            arrowPoint.getPoints().addAll(new Double[]{
-                    arrowBody.getEndX() - radius*0.5, arrowBody.getEndY() - 0.25*radius,
-                    arrowBody.getEndX(), arrowBody.getEndY(),
-                    arrowBody.getEndX() - radius*0.5, arrowBody.getEndY() + 0.25*radius
-            });
-        }
-        if(direction == Direction.Left){
-            arrowBody = new Line(circleX - radius, circleY + 0.25*radius, circleX - 3*radius, circleY + 0.25*radius);
-            arrowPoint.getPoints().addAll(new Double[]{
-                    arrowBody.getEndX() + radius*0.5, arrowBody.getEndY() - 0.25*radius,
-                    arrowBody.getEndX(), arrowBody.getEndY(),
-                    arrowBody.getEndX() + radius*0.5, arrowBody.getEndY() + 0.25*radius
-            });
-        }
-        if(direction == Direction.Down){
-            arrowBody = new Line(circleX + 0.25*radius, circleY + radius, circleX + 0.25*radius, circleY + 3*radius);
-            arrowPoint.getPoints().addAll(new Double[]{
-                    arrowBody.getEndX() - radius*0.25, arrowBody.getEndY() - 0.5*radius,
-                    arrowBody.getEndX(), arrowBody.getEndY(),
-                    arrowBody.getEndX() + radius*0.25, arrowBody.getEndY() - 0.5*radius
-            });
-        }
-        if(direction == Direction.Up){
-            arrowBody = new Line(circleX - 0.25*radius, circleY - radius, circleX - 0.25*radius, circleY - 3*radius);
-            arrowPoint.getPoints().addAll(new Double[]{
-                    arrowBody.getEndX() - radius*0.25, arrowBody.getEndY() + 0.5*radius,
-                    arrowBody.getEndX(), arrowBody.getEndY(),
-                    arrowBody.getEndX() + radius*0.25, arrowBody.getEndY() + 0.5*radius
-            });
-        }
-        graphPane.getChildren().add(arrowBody);
-        graphPane.getChildren().add(arrowPoint);
-    }
-
-    public void MinusClicked(ActionEvent actionEvent) {
-        Graph graph = Main.getGraph();
+    public void drawGraph(Graph graph, AnchorPane graphPane, Path[] paths){
         double vertexRadius;
         double scrollbarWidth = 15;
         double graphPaneWidth = graphPane.getWidth() - scrollbarWidth;
@@ -94,19 +57,72 @@ public class UIGraphViewController {
                     int neighbor = graph.getNeighbour((i*graph.getNumOfColumns() + n), direction);
                     if(neighbor >= 0){
                         if(direction == Direction.Right && graph.getVertices()[neighbor].weightRight >= 0)
-                            addArrow(graphPane, circleX, circleY, vertexRadius, direction);
+                            addArrow(graphPane, circleX, circleY, vertexRadius, direction, graph.getVertices()[neighbor].weightRight);
                         if(direction == Direction.Left && graph.getVertices()[neighbor].weightLeft >= 0)
-                            addArrow(graphPane, circleX, circleY, vertexRadius, direction);
+                            addArrow(graphPane, circleX, circleY, vertexRadius, direction, graph.getVertices()[neighbor].weightLeft);
                         if(direction == Direction.Up && graph.getVertices()[neighbor].weightUp >= 0)
-                            addArrow(graphPane, circleX, circleY, vertexRadius, direction);
+                            addArrow(graphPane, circleX, circleY, vertexRadius, direction, graph.getVertices()[neighbor].weightUp);
                         if(direction == Direction.Down && graph.getVertices()[neighbor].weightDown >= 0)
-                            addArrow(graphPane, circleX, circleY, vertexRadius, direction);
+                            addArrow(graphPane, circleX, circleY, vertexRadius, direction, graph.getVertices()[neighbor].weightDown);
                     }
                 }
                 graphPane.getChildren().add(vertexCircle);
                 graphPane.getChildren().add(vertexNumLabel);
             }
         }
+    }
+
+    private void addArrow(AnchorPane graphPane, double circleX, double circleY, double radius, Direction direction, double weight, Color color){
+        Line arrowBody = null;
+        Polygon arrowPoint = null;
+        arrowPoint = new Polygon();
+        Label weightLabel = new Label(new DecimalFormat("0.000000").format(weight));
+        weightLabel.setFont(Font.font("System", radius*0.35));
+        if(direction == Direction.Right){
+            arrowBody = new Line(circleX + radius, circleY - 0.25*radius, circleX + 3*radius, circleY - 0.25*radius);
+            arrowPoint.getPoints().addAll(arrowBody.getEndX() - radius*0.5, arrowBody.getEndY() - 0.25*radius,
+                    arrowBody.getEndX(), arrowBody.getEndY(),
+                    arrowBody.getEndX() - radius*0.5, arrowBody.getEndY() + 0.25*radius);
+            weightLabel.setLayoutX(arrowBody.getStartX());
+            weightLabel.setLayoutY(arrowBody.getStartY() - weightLabel.getFont().getSize() - 5);
+        }
+        if(direction == Direction.Left){
+            arrowBody = new Line(circleX - radius, circleY + 0.25*radius, circleX - 3*radius, circleY + 0.25*radius);
+            arrowPoint.getPoints().addAll(arrowBody.getEndX() + radius*0.5, arrowBody.getEndY() - 0.25*radius,
+                    arrowBody.getEndX(), arrowBody.getEndY(),
+                    arrowBody.getEndX() + radius*0.5, arrowBody.getEndY() + 0.25*radius);
+            weightLabel.setLayoutX(arrowBody.getEndX() + radius*0.5);
+            weightLabel.setLayoutY(arrowBody.getEndY());
+            weightLabel.setTextAlignment(TextAlignment.RIGHT);
+        }
+        if(direction == Direction.Down){
+            arrowBody = new Line(circleX + 0.25*radius, circleY + radius, circleX + 0.25*radius, circleY + 3*radius);
+            arrowPoint.getPoints().addAll(arrowBody.getEndX() - radius*0.25, arrowBody.getEndY() - 0.5*radius,
+                    arrowBody.getEndX(), arrowBody.getEndY(),
+                    arrowBody.getEndX() + radius*0.25, arrowBody.getEndY() - 0.5*radius);
+            weightLabel.setLayoutX(arrowBody.getStartX() - weightLabel.getFont().getSize());
+            weightLabel.setLayoutY(arrowBody.getStartY() + radius*0.5);
+            weightLabel.setRotate(90);
+        }
+        if(direction == Direction.Up){
+            arrowBody = new Line(circleX - 0.25*radius, circleY - radius, circleX - 0.25*radius, circleY - 3*radius);
+            arrowPoint.getPoints().addAll(arrowBody.getEndX() - radius*0.25, arrowBody.getEndY() + 0.5*radius,
+                    arrowBody.getEndX(), arrowBody.getEndY(),
+                    arrowBody.getEndX() + radius*0.25, arrowBody.getEndY() + 0.5*radius);
+            weightLabel.setLayoutX(arrowBody.getEndX()  - weightLabel.getFont().getSize() * 3);
+            weightLabel.setLayoutY(arrowBody.getEndY() + radius);
+            weightLabel.setRotate(-90);
+            weightLabel.setTextAlignment(TextAlignment.RIGHT);
+        }
+        weightLabel.setTextFill(Paint.valueOf("red"));
+        arrowBody.setFill(color);
+        graphPane.getChildren().add(arrowBody);
+        graphPane.getChildren().add(arrowPoint);
+        graphPane.getChildren().add(weightLabel);
+    }
+
+    public void MinusClicked(ActionEvent actionEvent) {
+        drawGraph(Main.getGraph(), graphPane, null);
     }
 
     public void PlusClicked(ActionEvent actionEvent) {
